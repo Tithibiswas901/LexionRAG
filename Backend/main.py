31bf088c34a9fc6fe19e4bdce9981703e9e1e4a2
Tithibiswas901 <tithibiswas2609@gmail.com>
2026-01-18 22:17:37 +0530

    fix

--- Backend/main.py
+++ Backend/main.py
@@ -161,3 +161,6 @@ async def ask_question(payload: dict):
         "answer": answer,
         "context_chunks": top_chunks
     }
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
